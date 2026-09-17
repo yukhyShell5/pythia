68a68851ff95ed470834703239799e4127176c4c
yukhyShell5 <zoulou.azerty33@gmail.com>
2026-09-17 20:23:53 +0200

    fix(web): fix unpkg MIME type issue and polyfill global variable in bundle

--- pythia.bundle.js
+++ pythia.bundle.js
@@ -7602,7 +7602,7 @@
       __exportStar2(require_types(), exports);
       __exportStar2(require_types_GENERATED(), exports);
       async function init(moduleOverrides = {}) {
-        const initZ3 = global.initZ3;
+        const initZ3 = window.initZ3;
         if (initZ3 === void 0) {
           throw new Error("initZ3 was not imported correctly. Please consult documentation on how to load Z3 in browser");
         }
